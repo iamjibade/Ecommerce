29ae9ed134795d71280eb298e36c96a63193fd79
Shittu Ajibade <68575764+iamjibade@users.noreply.github.com>
2025-06-09 00:50:37 +0100

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "welcome to Ecommerce!";
+  }
+  function myFunction(){
+    onclick="document.getElementById('demo').innerHTML = Date()";
+  }
+  function myFunction(){
+    onclick="document.getElementById('demo').innerHTML = "
+  }
